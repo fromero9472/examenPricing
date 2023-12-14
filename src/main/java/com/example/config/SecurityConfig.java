3584ece34f0c61fda129718224a2b8d50b8582fa
fromero9472 <86205844+fromero9472@users.noreply.github.com>
2023-12-14 05:10:05 +0100

    Update SecurityConfig.java

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -7,19 +7,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+/**
+ * Configuración de seguridad para la aplicación.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * Configuración de reglas de seguridad HTTP.
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Desactivar CSRF (Cross-Site Request Forgery) para simplificar
         http.csrf().disable()
+                // Permitir acceso sin autenticación a ciertos endpoints
                 .authorizeRequests().antMatchers("/generate-token", "/getPriceList").permitAll()
+                // Exigir autenticación para cualquier otra solicitud
                 .anyRequest().authenticated()
                 .and()
+                // Configurar la gestión de sesiones como sin estado (stateless)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    /**
+     * Bean para proporcionar el filtro de autenticación JWT.
+     */
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
         return new JwtAuthenticationFilter();
