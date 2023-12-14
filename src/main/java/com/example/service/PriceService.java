de95bd0071d96ccd6a26c982e7abe95d51f8070b
fromero9472 <86205844+fromero9472@users.noreply.github.com>
2023-12-14 05:10:47 +0100

    Update PriceService.java

--- src/main/java/com/example/service/PriceService.java
+++ src/main/java/com/example/service/PriceService.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -17,13 +18,39 @@ public class PriceService {
     @Autowired
     private PriceRepository priceRepository;
 
-    public List<Price> getPriceList(Long brandId, Long productId, LocalDateTime applicationDate) {
-        log.info("Getting price list for Brand ID {}, Product ID {}, Application Date {}", brandId, productId, applicationDate);
-        List<Price> prices = priceRepository.findByBrandIdAndProductIdAndStartDate(
-                brandId, productId, applicationDate);
-        log.info("Found {} prices", prices.size());
-        return prices;
+    public List<Price> getPriceList(Map<String, Object> params) {
+        try {
+            // Extraer parámetros del mapa
+            Long brandId = getLongParameter(params, "brandId");
+            Long productId = getLongParameter(params, "productId");
+            LocalDateTime applicationDate = parseStringToLocalDateTime((String) params.get("applicationDate"));
+
+            log.info("Getting price list for Brand ID {}, Product ID {}, Application Date {}", brandId, productId, applicationDate);
+
+            // Delegar al repositorio
+            List<Price> prices = priceRepository.findByBrandIdAndProductIdAndStartDate(brandId, productId, applicationDate);
+
+            log.info("Found {} prices", prices.size());
+            return prices;
+        } catch (Exception e) {
+            log.error("Error processing getPriceList request", e);
+            throw new RuntimeException("Error processing getPriceList request", e);
+        }
     }
 
+    // Métodos de utilidad
 
+    private Long getLongParameter(Map<String, Object> params, String paramName) {
+        Object paramValue = params.get(paramName);
+        if (paramValue instanceof Number) {
+            return ((Number) paramValue).longValue();
+        } else {
+            throw new IllegalArgumentException(paramName + " must be a number");
+        }
+    }
+
+    private LocalDateTime parseStringToLocalDateTime(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
+        return LocalDateTime.parse(dateString, formatter);
+    }
 }
