60c019b67d5d1bc9d82c5a39e2d60645f554c2d4
fromero9472 <86205844+fromero9472@users.noreply.github.com>
2023-12-14 05:11:10 +0100

    Update PriceServiceTest.java

--- src/test/java/service/PriceServiceTest.java
+++ src/test/java/service/PriceServiceTest.java
@@ -3,65 +3,60 @@ package service;
 import com.example.model.Price;
 import com.example.repository.PriceRepository;
 import com.example.service.PriceService;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-// Extiende con la anotación MockitoExtension para utilizar Mockito en las pruebas
 @ExtendWith(MockitoExtension.class)
 public class PriceServiceTest {
 
-    // Mock del repositorio para simular su comportamiento
     @Mock
     private PriceRepository priceRepository;
 
-    // Instancia del servicio a probar, con los mocks inyectados automáticamente
     @InjectMocks
     private PriceService priceService;
 
-    // Prueba parametrizada con diferentes conjuntos de datos
     @ParameterizedTest
     @CsvSource({
-            "1, 35455, 2023-12-14-10.00.00",
-            "1, 35455, 2023-12-14-16.00.00",
-            "1, 35455, 2023-12-14-21.00.00",
-            "1, 35455, 2023-12-15-10.00.00",
-            "1, 35455, 2023-12-16-21.00.00"
+            "1, 35455, 2023-12-14-10.00.00, 1",
+            "1, 35455, 2023-12-14-16.00.00, 1",
+            "1, 35455, 2023-12-14-21.00.00, 1",
+            "1, 35455, 2023-12-15-10.00.00, 1",
+            "1, 35455, 2023-12-16-21.00.00, 1"
     })
-    public void testGetPriceList(Long brandId, Long productId, String dateTime) {
-        // Llama al método de prueba con los parámetros de entrada
-        executeTest(brandId, productId, dateTime, () -> Collections.singletonList(new Price()));
-    }
-
-    // Método que realiza la ejecución de la prueba
-    private void executeTest(Long brandId, Long productId, String dateTime, Supplier<List<Price>> serviceResponse) {
-        // Convierte la cadena de fecha y hora a LocalDateTime
+    public void testGetPriceList(Long brandId, Long productId, String dateTime, int invocationTimes) {
+        // Convertir la cadena de fecha y hora a LocalDateTime
         LocalDateTime applicationDate = parseStringToLocalDateTime(dateTime);
 
-        // Configura el comportamiento simulado del repositorio
+        // Configurar el comportamiento simulado del repositorio
         when(priceRepository.findByBrandIdAndProductIdAndStartDate(brandId, productId, applicationDate))
-                .thenReturn(serviceResponse.get());
+                .thenReturn(Collections.singletonList(new Price()));
 
-        // Llama al método del servicio bajo prueba
-        List<Price> result = priceService.getPriceList(brandId, productId, applicationDate);
+        // Llamar al método del servicio bajo prueba
+        Map<String, Object> params = new HashMap<>();
+        params.put("brandId", brandId);
+        params.put("productId", productId);
+        params.put("applicationDate", dateTime);
 
-        // Verifica que el repositorio fue llamado con los parámetros correctos
-        verify(priceRepository, times(1)).findByBrandIdAndProductIdAndStartDate(brandId, productId, applicationDate);
+        List<Price> result = priceService.getPriceList(params);
+        // Verificar que el repositorio fue llamado con los parámetros correctos
+        verify(priceRepository, times(invocationTimes)).findByBrandIdAndProductIdAndStartDate(brandId, productId, applicationDate);
 
-        // Verifica que el resultado del servicio coincide con la respuesta simulada del repositorio
-        assertEquals(serviceResponse.get(), result);
+        // Verificar que el resultado del servicio coincide con la respuesta simulada del repositorio
+        assertEquals(Collections.singletonList(new Price()), result);
     }
 
     // Convierte una cadena de fecha y hora a LocalDateTime
